6a9bbc8ac52b64c4d892043d39bfa18c3c8a8dd9
unknown <riasat01@outlook.com>
2023-09-18 04:06:34 +0600

    responsive navbar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import Link from "./Link";
-
+import { AiOutlineMenuUnfold, AiOutlineClose } from 'react-icons/ai'
 const Navbar = () => {
+
+    const [open, setOpen] = useState(false);
 
     const fakeRoutes = [
         { id: 1, path: '/', name: 'Home' },
@@ -8,15 +11,23 @@ const Navbar = () => {
         { id: 3, path: '/user', name: 'User Profile', routes: [{ id: 4, path: '/user/profile', name: 'User Profile Page' }, { id: 5, path: '/user/settings', name: 'User Settings Page' }] },
         { id: 6, path: '/dashboard', name: 'Dashboard', private: true },
         { id: 7, path: '/blog', name: 'Blog List' }
-      ];
+    ];
 
-      
+
     return (
-        <ul className="flex gap-16">
-          {
-            fakeRoutes.map(fakeRoute => <Link key={fakeRoute.id} fakeRoute={fakeRoute}></Link>)
-          }  
-        </ul>
+        <div className="">
+            <div onClick={() => setOpen(!open)} className="text-2xl lg:hidden">
+                {
+                    open === true ? <AiOutlineClose></AiOutlineClose> : <AiOutlineMenuUnfold></AiOutlineMenuUnfold>
+                }
+            </div>
+            <ul className={`absolute lg:relative top-6 lg:top-0 duration-1000 ${open === true ? `left-8` : `-left-28`} lg:left-0 lg:flex lg:gap-16`}>
+                {
+                    fakeRoutes.map(fakeRoute => <Link key={fakeRoute.id} fakeRoute={fakeRoute}></Link>)
+                }
+            </ul>
+        </div>
+
     );
 };
 
